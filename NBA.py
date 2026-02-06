6b23d0e3e148f4661f5ecbd1bb7d2e38cf531fde
VaishnavYogesh807 <vaishnavayogesh1@gmail.com>
2026-02-05 19:17:54 -0500

    Update NBA.py

--- NBA.py
+++ NBA.py
@@ -7,13 +7,13 @@ data = np.genfromtxt(
 player_names = data['Player']
 seasons = data['Season']
 
-FG_acc = np.where(data['FGA'] > 0, data['FGM'] / data['FGA'], 0)
-TP_acc = np.where(data['3PA'] > 0, data['3PM'] / data['3PA'], 0)
-FT_acc = np.where(data['FTA'] > 0, data['FTM'] / data['FTA'], 0)
-results1 = np.column_stack((player_names, seasons, FG_acc, TP_acc, FT_acc))
+fg_acc = np.where(data['FGA'] > 0, data['FGM'] / data['FGA'], 0)
+tp_acc = np.where(data['3PA'] > 0, data['3PM'] / data['3PA'], 0)
+ft_acc = np.where(data['FTA'] > 0, data['FTM'] / data['FTA'], 0)
+results1 = np.column_stack((player_names, seasons, fg_acc, tp_acc, ft_acc))
 
-avg_pts_mid = np.where(data['MIN'] > 0, data['PTS'] / data['MIN'], 0)
-results2 = np.column_stack((player_names, seasons, avg_pts_mid))
+avg_pts_min = np.where(data['MIN'] > 0, data['PTS'] / data['MIN'], 0)
+results2 = np.column_stack((player_names, seasons, avg_pts_min))
 
 overall_acc = np.where(data['FGA'] + data['3PA'] + data['FTA'] > 0,
                         (data['FGM'] + data['3PM'] + data['FTM']) /
@@ -33,10 +33,10 @@ def top100(metric, label=""):
         print(f"Player {pos}: {player_names[i]}, Season: {seasons[i]}, {label}: {metric[i]}")
         pos += 1
 
-top100(FG_acc, "FG Accuracy")
-top100(TP_acc, "3PT Accuracy")
-top100(FT_acc, "FT Accuracy")
-top100(avg_pts_mid, "Average Points per Minute")
+top100(fg_acc, "FG Accuracy")
+top100(tp_acc, "3PT Accuracy")
+top100(ft_acc, "FT Accuracy")
+top100(avg_pts_min, "Average Points per Minute")
 top100(overall_acc, "Overall Shooting Accuracy")
 top100(blocks, "Blocks per Game")
 top100(steals, "Steals per Game")
